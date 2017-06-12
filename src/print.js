040a564912d43261f4b608a990cbb2df7d50854e
Alex Chalk <alexchalk17@gmail.com>
2017-06-12 11:58:10 -0400

    printStatement: add zeros to output

--- src/print.js
+++ src/print.js
@@ -15,18 +15,26 @@
     return date.getDate() + '/' + month(date) + '/' + date.getFullYear();
   };
     
+  Number.prototype.addZeros = function(){
+    if (this === Math.round(this)) {
+      return this + '.00';
+    } else {
+      return this;
+    }
+  };
+
   var formatAmount = function(amount){
     if (amount < 0) {
-      return '|| ' + Math.abs(amount);
+      return '|| ' + Math.abs(amount).addZeros();
     } else {
-      return amount + ' ||';
+      return amount.addZeros() + ' ||';
     }
   };
 
   var printStatement = function() {
     console.log(header);
     this.account.history.reverse().forEach(function(transaction) {
-      console.log(formatDate(transaction.timestamp) + ' || ' + formatAmount(transaction.amount) + ' || ' + transaction.balance);
+      console.log(formatDate(transaction.timestamp) + ' || ' + formatAmount(transaction.amount) + ' || ' + transaction.balance.addZeros());
     });
   };
   module.exports = printStatement;
--- test/printSpec.js
+++ test/printSpec.js
@@ -10,52 +10,48 @@ describe('BANKAPP.printStatement', function() {
   before(function() {
     header = 'date || credit || debit || balance';
     sinon.spy(console, 'log');
-  });
-  beforeEach(function() {
     BANKAPP.init();
     account = BANKAPP.account;
     clock = sinon.useFakeTimers(new Date(2012,1,10).getTime());
     account.processTransaction(1000); clock.tick(1000 * 60 * 60 * 72);
     account.processTransaction(2000); clock.tick(1000 * 60 * 60 * 24);
     account.processTransaction(-500);
+    BANKAPP.printStatement();
   });
   after(function() {
     clock.restore();
   });
 
   it('prints a descriptive header', function() {
-    BANKAPP.printStatement();
     expect(console.log.getCall(0).args[0]).to.equal(header);
   });
 
   it('prints date of transactions', function() {
-    BANKAPP.printStatement();
     expect(console.log.getCall(1).args[0]).to.include('14/01/2012');
   });
 
+  it('prints amounts with two decimal places', function() {
+    expect(console.log.getCall(3).args[0]).to.include('1000.00');
+  });
+
   it('prints amount of transactions', function() {
-    BANKAPP.printStatement();
-    expect(console.log.getCall(3).args[0]).to.include('1000');
+    expect(console.log.getCall(3).args[0]).to.include('1000.00');
   });
 
   it('always prints amount as positive number', function() {
-    BANKAPP.printStatement();
-    expect(console.log.getCall(1).args[0]).to.include('|| 500');
+    expect(console.log.getCall(1).args[0]).to.include('|| 500.00');
   });
 
   it('prints credits in credit column', function() {
-    BANKAPP.printStatement();
-    expect(console.log.getCall(2).args[0]).to.include('2000 || || ');
+    expect(console.log.getCall(2).args[0]).to.include('2000.00 || || ');
   });
 
   it('prints debits in debit column', function() {
-    BANKAPP.printStatement();
-    expect(console.log.getCall(1).args[0]).to.include('|| || 500');
+    expect(console.log.getCall(1).args[0]).to.include('|| || 500.00');
   });
 
   it('prints balance after each transaction', function() {
-    BANKAPP.printStatement();
-    expect(console.log.getCall(1).args[0]).to.include('2500');
+    expect(console.log.getCall(1).args[0]).to.include('2500.00');
   });
 });
 
